e9de906f4852fb98876184174acd51a84cdea86b
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2022-07-04 12:11:46 +0200

    add:query and AllBooks

--- server/server.js
+++ server/server.js
@@ -6,3 +6,47 @@ const { readFileSync } = require('fs')
 
 const schemaString = readFileSync('./schema.graphql', { encoding: 'utf8' })
 const schema = buildSchema(schemaString)
+
+const allBooks = [
+  {
+    id: '1',
+    title: 'Book 1',
+    description: 'Book description 1',
+    author: {
+      id: '1',
+      firstName: 'Sergii',
+      lastName: 'Velikorodnov'
+    }
+  },
+  {
+    id: '2',
+    title: 'Book 2',
+    description: 'Book description 2',
+    author: {
+      id: '1',
+      firstName: 'Sergii',
+      lastName: 'Velikorodnov'
+    }
+  }
+]
+
+const root = {
+  getAllBooks: () => {
+    return allBooks
+  },
+  getBook: params => {
+    return allBooks.find(({ id }) => params.id === id)
+  },
+  addBook: params => {
+    allBooks.push({
+      id: allBooks.length + 1,
+      ...params.book,
+      author: {
+        id: '1',
+        firstName: 'Sergii',
+        lastName: 'Velikorodnov'
+      }
+    })
+    return true
+  }
+}
